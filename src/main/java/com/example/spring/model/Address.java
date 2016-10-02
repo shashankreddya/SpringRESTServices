5211937818b4273535865695c5bf555e69d71e24
Shashank <shashankreddy16@gmail.com>
2016-10-02 00:26:23 -0500

    Changed address model class

--- src/main/java/com/example/spring/model/Address.java
+++ src/main/java/com/example/spring/model/Address.java
@@ -1,21 +1,51 @@
 package com.example.spring.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Address")
 public class Address {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id", nullable=false, unique=true, length=11)
+	private int id;
+	
+	@Column(name="line1", length=40, nullable=true)
 	private String line1;
+	
+	@Column(name="city", length=20, nullable=true)
 	private String city;
+	
+	@Column(name="state", length=20, nullable=true)
 	private String state;
+	
+	@Column(name="zipcode", length=6, nullable=true)
 	private String zipCode;
 	
 	public Address() {
 	}
 	
-	public Address(String line1, String city, String state, String zipCode) {
+	public Address(int id, String line1, String city, String state, String zipCode) {
 		super();
+		this.id = id;
 		this.line1 = line1;
 		this.city = city;
 		this.state = state;
 		this.zipCode = zipCode;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public String getLine1() {
@@ -52,6 +82,8 @@ public class Address {
 
 	@Override
 	public String toString() {
-		return "Address [line1=" + line1 + ", city=" + city + ", state=" + state + ", zipCode=" + zipCode + "]";
-	}	
+		return "Address [id=" + id + ", line1=" + line1 + ", city=" + city + ", state=" + state + ", zipCode=" + zipCode
+				+ "]";
+	}
+
 }
